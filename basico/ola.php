ccc35e2634795bb83d8f8731ace7aacb880e917d
GabrielMBoeira <gboeira777@gmail.com>
2020-03-09 22:04:09 -0300

    adicionando arquivos

---
+++ basico/ola.php
@@ -0,0 +1,16 @@
+<div class="titulo">
+    Olá PHP!
+</div>
+
+<h6>Exemplo A</h6>
+<?php
+    echo 'olá <br>';
+    echo "mundo!";
+?>
+
+<h5>Exemplo B</h5>
+<?= " Outra forma de expressar "?>
+
+<?php
+
+phpinfo();
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Curso PHP</title>
-    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
 </head>
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <H2>índice dos exercícios</H2>
+        <h2>Índice dos Exercícios</h2>
     </header>
     <main class="principal">
         <div class="conteudo">
@@ -17,78 +17,18 @@
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo vermelho">
-                    <h3>Módulo 02</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo azul">
-                    <h3>Módulo 03</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo roxo">
-                    <h3>Módulo 04</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo laranja">
-                    <h3>Módulo 05</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo verde-escuro">
-                    <h3>Módulo 06</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo vermelho-escuro">
-                    <h3>Módulo 07</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo azul-escuro">
-                    <h3>Módulo 08</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
-                    </ul>
-                </div>
-                <div class="modulo roxo-escuro">
-                    <h3>Módulo 09</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>    
-                        <li><a href="#">Exercício B</a></li>    
-                        <li><a href="#">Exercício C</a></li>    
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=CSS">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a></li>
                     </ul>
                 </div>
             </nav>
         </div>
     </main>
-    <footer class="rodape">CODER & ALUNOS <?= date('Y'); ?></footer>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
 </body>
-</html>
+</html> 
