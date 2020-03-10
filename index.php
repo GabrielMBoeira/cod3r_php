385b78bcae44de24ba5d89a611e9d2b556f4643f
GabrielMBoeira <gboeira777@gmail.com>
2020-03-09 23:42:16 -0300

    adicinando arquivos

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
     <title>Curso PHP</title>
 </head>
+
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
@@ -15,13 +17,21 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=CSS">Integração CSS</a></li>
                         <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Inteiro</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=float">Float</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=aritimeticas">Op. aritiméticas</a></li>
                     </ul>
                 </div>
             </nav>
@@ -31,4 +41,5 @@
         COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
-</html> 
+
+</html>
---
+++ tipos/aritimeticas.php
@@ -0,0 +1,17 @@
+<div class="titulo">Operações Aritiméticas</div>
+
+<?php
+
+echo 1 + 1, '<br>';
+
+var_dump(1 + 1);
+echo '<br>';
+
+echo 1 + 2.5, '<br>';
+echo 10 - 2, '<br>';
+echo 2 * 5, '<br>';
+echo 7 / 4, '<br>'; //mesmo dividindo inteiros resultado será um float
+echo intdiv(7, 4), '<br>'; //Irá retornar um inteiro (retira as casas decimal)
+echo round(7 / 4), '<br>'; // Irá arredondar ou pra cima ou para baixo
+echo 7 % 4, '<br>' ;
+
